22f245d3d50e44ec4de6d7b0ee928be0cf7569ee
lakshmilachu <lachucse1994@gmail.com>
2022-07-16 21:24:36 +0530

    swiper js

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-
+import { AfterContentChecked, Component, ViewChild } from '@angular/core';
+import { SwiperComponent } from 'swiper/angular';
+import SwiperCore, { Autoplay, Pagination } from 'swiper';
+SwiperCore.use([Pagination, Autoplay]);
+// SwiperCore.use([Autoplay, Keyboard, Pagination, Scrollbar, Zoom, IonicSlides]);
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('swiper') swiper: SwiperComponent;
 
   constructor() {}
-
+  // ngAfterContentChecked(): void {
+  //   if (this.swiper) {
+  //     this.swiper.updateSwiper({});
+  //   }
+  // }
 }
