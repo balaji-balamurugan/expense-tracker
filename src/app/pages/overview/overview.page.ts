d055437b1d44ad89742a056491b985473354d8a5
balajibadly <balajibalamurugan236@gmail.com>
2022-07-18 07:01:09 +0530

    fix(overview page): minor ui fixes and removed unused oninit
    use oninit when required otherwise remove it.

--- src/app/pages/overview/overview.page.ts
+++ src/app/pages/overview/overview.page.ts
@@ -3,14 +3,12 @@ import { SwiperComponent } from 'swiper/angular';
 import SwiperCore, { Autoplay, Pagination } from 'swiper';
 
 SwiperCore.use([Pagination, Autoplay]);
+
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.page.html',
   styleUrls: ['./overview.page.scss'],
 })
-export class OverviewPage implements OnInit {
+export class OverviewPage {
   @ViewChild('swiper') swiper: SwiperComponent;
-  constructor() {}
-
-  ngOnInit() {}
 }
