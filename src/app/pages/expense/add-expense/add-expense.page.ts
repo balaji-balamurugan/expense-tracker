3e917329208f1f1d53235bc1f70149f401045fa3
lakshmilachu <lachucse1994@gmail.com>
2022-07-26 22:09:15 +0530

    add date picker expense and income page

--- src/app/pages/expense/add-expense/add-expense.page.ts
+++ src/app/pages/expense/add-expense/add-expense.page.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
+import { format, parseISO } from 'date-fns';
 import { DatePickerComponent } from 'src/app/shared/components/date-picker/date-picker.component';
-
 @Component({
   selector: 'et-add-expense',
   templateUrl: './add-expense.page.html',
@@ -12,6 +12,10 @@ export class AddExpensePage implements OnInit {
   expenseForm!: FormGroup;
   showDatePicker = false;
 
+  formattedString: any;
+  dateValue: any;
+  datetime: any;
+
   constructor(public modalController: ModalController) { }
 
   ngOnInit() {
@@ -20,6 +24,11 @@ export class AddExpensePage implements OnInit {
       title: new FormControl(''),
       amount: new FormControl(null),
     });
+  }
+
+  modalDateChanged(value: any) {
+    this.dateValue = value;
+    this.formattedString = format(parseISO(value), 'MMM d, yyyy');
   }
 
   expense() {
