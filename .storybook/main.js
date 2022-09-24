351a3d76101ad0c193e98e58438ca83e4985c86d
balajibadly <balajibalamurugan236@gmail.com>
2022-09-24 08:39:34 +0530

    chore(storybook): :sparkles: added default viewport for storybook
    default view ports from storybook for different viewport checking.
    
    none

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,9 +2,9 @@ module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
+    "@storybook/addon-viewport",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-viewport",
     "@storybook/addon-actions",
   ],
   framework: "@storybook/angular",
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,12 @@
 import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 setCompodocJson(docJson);
 
 export const parameters = {
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
