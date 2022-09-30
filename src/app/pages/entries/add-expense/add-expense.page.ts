b546a1069c34547cede23bd5fbb80323ff418064
balajibadly <balajibalamurugan236@gmail.com>
2022-09-30 09:47:50 +0530

    refactor(add expense form): :hammer: update form values to firestore
    save expense to expenses collection
    
    noen

--- src/app/pages/entries/add-expense/add-expense.page.ts
+++ src/app/pages/entries/add-expense/add-expense.page.ts
@@ -1,35 +1,35 @@
 import { Component } from '@angular/core';
-import { FormGroup, NonNullableFormBuilder } from '@angular/forms';
+import { NonNullableFormBuilder } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
+import { expensePath } from 'src/app/shared/constants/firepath.constant';
+import { FirestoreService } from 'src/app/shared/services/firestore.service';
+
 @Component({
   selector: 'et-add-expense',
   templateUrl: './add-expense.page.html',
   styleUrls: ['./add-expense.page.scss'],
 })
 export class AddExpensePage {
-  expenseForm!: FormGroup;
+  expenseForm = this.fb.group({
+    date: this.fb.control(''),
+    title: this.fb.control(''),
+    amount: this.fb.control(null),
+  });
 
   constructor(
     private fb: NonNullableFormBuilder,
     public modalController: ModalController,
+    private firestore: FirestoreService,
   ) {
-    this.expenseForm = this.fb.group({
-      date: this.fb.control(''),
-      title: this.fb.control(''),
-      amount: this.fb.control(null),
-    });
-
-    this.expenseForm.get('date')?.valueChanges
-      .subscribe((date) => {
-        // this.expenseForm.get('date')?.setValue(
-        //   new Date(date as string) as Date,
-        //   { emitEvent: false }
-        // );
-      });
+    this.expenseForm.get('date')?.valueChanges.subscribe(console.log);
   }
 
-  expense() {
-    console.log(this.expenseForm.value);
+  async addExpense() {
+    if (this.expenseForm.status === 'INVALID') {
+      return;
+    }
+    await this.firestore.add(expensePath, this.expenseForm.value);
+    this.modalController.dismiss();
   }
 
 }
