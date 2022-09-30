5c1dd9ee03e18739c2a7fce3d29a714b71568251
balajibadly <balajibalamurugan236@gmail.com>
2022-09-30 09:50:05 +0530

    fix(add income page): :hammer: updated formvalues in income to firestore
    saved formvalues to income collection in firestore
    
    none

--- src/app/pages/entries/add-income/add-income.page.ts
+++ src/app/pages/entries/add-income/add-income.page.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
+import { FormControl, NonNullableFormBuilder, Validators } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
 import { incomePath } from 'src/app/shared/constants/firepath.constant';
 import { FirestoreService } from 'src/app/shared/services/firestore.service';
@@ -17,39 +17,25 @@ export class AddIncomePage {
     subHeader: 'Select your favorite color',
   };
 
-  incomeForm: FormGroup<{
-    date: FormControl<Date | string | null>;
-    title: FormControl<string>;
-    amount: FormControl<number | null>;
-  }>;
+  incomeForm = this.fb.group({
+    date: this.fb.control(
+      null, Validators.required
+    ) as FormControl<Date | string | null>,
+    title: this.fb.control('', Validators.required),
+    amount: this.fb.control(null, Validators.required) as FormControl<number | null>,
+  });
 
   constructor(
     public modalController: ModalController,
     private fb: NonNullableFormBuilder,
-    private _firestore: FirestoreService
-  ) {
-    this.incomeForm = this.fb.group({
-      date: this.fb.control(
-        null, Validators.required
-      ) as FormControl<Date | string | null>,
-      title: this.fb.control('', Validators.required),
-      amount: this.fb.control(null, Validators.required) as FormControl<number | null>,
-    });
+    private firestore: FirestoreService
+  ) { }
 
-    this.incomeForm.get('date')?.valueChanges
-      .subscribe((date) => {
-        // this.incomeForm.get('date')?.setValue(
-        //   new Date(date as string) as Date,
-        //   { emitEvent: false }
-        // );
-      });
-  }
-
-  async submitForm() {
+  async addIncome() {
     if (this.incomeForm.status === 'INVALID') {
       return;
     }
-    await this._firestore.add(incomePath, this.incomeForm.value);
+    await this.firestore.add(incomePath, this.incomeForm.value);
     this.modalController.dismiss();
   }
 
